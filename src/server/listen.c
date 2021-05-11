46cbff5432b09dd1a3e443acbf8df365893fbcb7
charles.duchamp <charles.duchamp@epita.fr>
2021-05-12 01:46:15 +0200

    parsing dns request (Finished header parsing)

--- src/server/listen.c
+++ src/server/listen.c
@@ -14,19 +14,6 @@
 
 #include "parsing/request.h"
 
-/*
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0')
-*/
-
 void server_listen(server_config *cfg)
 {
     struct addrinfo *res = NULL, *rp = NULL;
@@ -83,15 +70,12 @@ void server_listen(server_config *cfg)
                 bs[c++] = byte;
                 bs[c] = '\0';
             }
-            //printf(""BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(client_message[i]));
         }
         puts("");
 
-        // parse dns request
+        // Parse DNS request
         parse_request(bs, c);
 
         sendto(sockfd, client_message, strlen(client_message), 0, (struct sockaddr *)&client, (socklen_t)c);
-
-//        string_free(bits);
     }
 }
