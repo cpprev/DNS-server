913cde5c67b6ed356544c53c52e3bbbc472e6f29
charles.duchamp <charles.duchamp@epita.fr>
2021-05-20 21:10:22 +0200

    fix cmake

--- tests/src/config.sh
+++ tests/src/config.sh
@@ -1,11 +1,9 @@
 echo -e "\n$red""[Config Files Tests]"
 
-################################################################################
-
 # Valid
 
-test_func_conf "valid" "valid zone" "input/valid/zone1.zone"
+test_func_conf "valid" "valid zone" "input/valid/valid1.zone"
 
 # Invalid
 
-test_func_conf "invalid" "invalid zone" "input/invalid/zone1.zone"
+test_func_conf "invalid" "invalid zone" "input/invalid/invalid1.zone"
