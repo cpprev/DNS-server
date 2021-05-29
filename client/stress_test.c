5e9fea4657a0546ed5a297a06a21fc019df37c19
charles.duchamp <charles.duchamp@epita.fr>
2021-05-29 19:39:08 +0200

    stress test multithread

--- client/stress_test.c
+++ client/stress_test.c
@@ -1,12 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <pthread.h>
 
 #include "stress_test.h"
 #include "connect.h"
 
-#define THREAD_CAP 6
+#define THREAD_CAP 16
+
+void *udp_routine()
+{
+    while (true)
+    {
+        udp_send_request();
+    }
+}
+
+void *tcp_routine()
+{
+    while (true)
+    {
+        tcp_send_request();
+    }
+}
+
+void thread_pool_stress_test(PROTOCOL proto)
+{
+    pthread_t tid[THREAD_CAP + 1];
+    int i = 0;
+    while (i < THREAD_CAP)
+    {
+        int p = 0;
+        if (proto == UDP)
+            p = pthread_create(&tid[i++], NULL, udp_routine, NULL);
+        else
+            p = pthread_create(&tid[i++], NULL, tcp_routine, NULL);
+        if (p != 0 || errno == EAGAIN)
+        {
+            --i;
+            break;
+        }
+    }
+
+    while (true)
+        printf("NB requests sent: %d\n", g_nb_req);
+}
 
 void basic_stress_test(PROTOCOL proto)
 {
@@ -16,32 +55,6 @@ void basic_stress_test(PROTOCOL proto)
             udp_send_request(g_message, g_ip, g_port);
         else if (proto == TCP)
             tcp_send_request(g_message, g_ip, g_port);
-        printf("NB requests sent: %d\n", g_nb_req);
-    }
-}
-
-void thread_pool_stress_test(PROTOCOL proto)
-{
-    (void) proto;
-
-    pthread_t tid[THREAD_CAP + 1];
-    int i = 0;
-
-    while (true)
-    {
-        int p = pthread_create(&tid[i++], NULL, udp_send_request, NULL);
-        printf("%d\n", p);
-        if (p != 0)
-            perror("thread create error, reduce number of threads.");
-        if (i >= THREAD_CAP - 1)
-        {
-            i = 0;
-            while (i < THREAD_CAP - 1)
-            {
-                if (pthread_join(tid[i++],NULL) != 0)
-                    perror("thread join error, reduce number of threads.");
-            }
-        }
         printf("NB requests sent: %d\n", g_nb_req);
     }
 }
